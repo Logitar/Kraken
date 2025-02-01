0d4588b0849814b02d0d615beab719ecaa38a39c
Francis Pion <francispion@hotmail.com>
2025-02-01 14:49:31 -0500

    session

---
+++ backend/src/Logitar.Kraken.Core/Sessions/Events/SessionCreated.cs
@@ -0,0 +1,8 @@
+﻿using Logitar.EventSourcing;
+using Logitar.Kraken.Core.Passwords;
+using Logitar.Kraken.Core.Users;
+using MediatR;
+
+namespace Logitar.Kraken.Core.Sessions.Events;
+
+public record SessionCreated(UserId UserId, Password? Secret) : DomainEvent, INotification;
---
+++ backend/src/Logitar.Kraken.Core/Sessions/IncorrectSessionSecretException.cs
@@ -0,0 +1,48 @@
+﻿namespace Logitar.Kraken.Core.Sessions;
+
+public class IncorrectSessionSecretException : InvalidCredentialsException
+{
+  private const string ErrorMessage = "The specified secret did not match the session.";
+
+  public Guid? RealmId
+  {
+    get => (Guid?)Data[nameof(RealmId)];
+    private set => Data[nameof(RealmId)] = value;
+  }
+  public Guid SessionId
+  {
+    get => (Guid)Data[nameof(SessionId)]!;
+    private set => Data[nameof(SessionId)] = value;
+  }
+  public string AttemptedSecret
+  {
+    get => (string)Data[nameof(AttemptedSecret)]!;
+    private set => Data[nameof(AttemptedSecret)] = value;
+  }
+
+  public override Error Error
+  {
+    get
+    {
+      Error error = new(this.GetErrorCode(), ErrorMessage);
+      error.Data[nameof(RealmId)] = RealmId;
+      error.Data[nameof(SessionId)] = SessionId;
+      error.Data[nameof(AttemptedSecret)] = AttemptedSecret;
+      return error;
+    }
+  }
+
+  public IncorrectSessionSecretException(Session session, string attemptedSecret)
+    : base(BuildMessage(session, attemptedSecret))
+  {
+    RealmId = session.Id.RealmId?.ToGuid();
+    SessionId = session.Id.EntityId;
+    AttemptedSecret = attemptedSecret;
+  }
+
+  private static string BuildMessage(Session session, string attemptedSecret) => new ErrorMessageBuilder(ErrorMessage)
+    .AddData(nameof(RealmId), session.Id.RealmId?.ToGuid())
+    .AddData(nameof(SessionId), session.Id.EntityId)
+    .AddData(nameof(AttemptedSecret), attemptedSecret)
+    .Build();
+}
