b4e521f713d606a0c6b9ba83078c2751fe58f9cc
Francis Pion <francispion@hotmail.com>
2025-02-01 21:26:34 -0500

    Added some controllers. (#15)

---
+++ backend/src/Logitar.Kraken.Web/Controllers/ApiKeyController.cs
@@ -0,0 +1,84 @@
+﻿using Logitar.Kraken.Contracts.ApiKeys;
+using Logitar.Kraken.Contracts.Search;
+using Logitar.Kraken.Core.ApiKeys.Commands;
+using Logitar.Kraken.Core.ApiKeys.Queries;
+using Logitar.Kraken.Web.Models.ApiKey;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logitar.Kraken.Web.Controllers;
+
+[ApiController]
+[Route("api/keys")]
+public class ApiKeyController : ControllerBase
+{
+  private readonly IMediator _mediator;
+
+  public ApiKeyController(IMediator mediator)
+  {
+    _mediator = mediator;
+  }
+
+  [HttpPatch("authenticate")]
+  public async Task<ActionResult<ApiKeyModel>> AuthenticateAsync([FromBody] AuthenticateApiKeyPayload payload, CancellationToken cancellationToken)
+  {
+    AuthenticateApiKeyCommand command = new(payload);
+    ApiKeyModel apiKey = await _mediator.Send(command, cancellationToken);
+    return Ok(apiKey);
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<ApiKeyModel>> CreateAsync([FromBody] CreateOrReplaceApiKeyPayload payload, CancellationToken cancellationToken)
+  {
+    CreateOrReplaceApiKeyCommand command = new(Id: null, payload, Version: null);
+    CreateOrReplaceApiKeyResult result = await _mediator.Send(command, cancellationToken);
+    return ToActionResult(result);
+  }
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<ApiKeyModel>> ReadAsync(Guid id, CancellationToken cancellationToken)
+  {
+    ReadApiKeyQuery command = new(id);
+    ApiKeyModel? apiKey = await _mediator.Send(command, cancellationToken);
+    return apiKey == null ? NotFound() : Ok(apiKey);
+  }
+
+  [HttpPut("{id}")]
+  public async Task<ActionResult<ApiKeyModel>> ReplaceAsync(Guid id, [FromBody] CreateOrReplaceApiKeyPayload payload, long? version, CancellationToken cancellationToken)
+  {
+    CreateOrReplaceApiKeyCommand command = new(id, payload, version);
+    CreateOrReplaceApiKeyResult result = await _mediator.Send(command, cancellationToken);
+    return ToActionResult(result);
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<SearchResults<ApiKeyModel>>> SearchAsync([FromQuery] SearchApiKeysParameters parameters, CancellationToken cancellationToken)
+  {
+    SearchApiKeysPayload payload = parameters.ToPayload();
+    SearchApiKeysQuery query = new(payload);
+    SearchResults<ApiKeyModel> apiKeys = await _mediator.Send(query, cancellationToken);
+    return Ok(apiKeys);
+  }
+
+  [HttpPatch("{id}")]
+  public async Task<ActionResult<ApiKeyModel>> UpdateAsync(Guid id, [FromBody] UpdateApiKeyPayload payload, CancellationToken cancellationToken)
+  {
+    UpdateApiKeyCommand command = new(id, payload);
+    ApiKeyModel? apiKey = await _mediator.Send(command, cancellationToken);
+    return apiKey == null ? NotFound() : Ok(apiKey);
+  }
+
+  private ActionResult<ApiKeyModel> ToActionResult(CreateOrReplaceApiKeyResult result)
+  {
+    if (result.ApiKey == null)
+    {
+      return NotFound();
+    }
+    else if (result.Created)
+    {
+      Uri uri = new($"{Request.Scheme}://{Request.Host}/api/keys/{result.ApiKey.Id}", UriKind.Absolute);
+      return Created(uri, result.ApiKey);
+    }
+    return Ok(result.ApiKey);
+  }
+}
---
+++ backend/src/Logitar.Kraken.Web/Controllers/RoleController.cs
@@ -0,0 +1,84 @@
+﻿using Logitar.Kraken.Contracts.Roles;
+using Logitar.Kraken.Contracts.Search;
+using Logitar.Kraken.Core.Roles.Commands;
+using Logitar.Kraken.Core.Roles.Queries;
+using Logitar.Kraken.Web.Models.Role;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logitar.Kraken.Web.Controllers;
+
+[ApiController]
+[Route("api/roles")]
+public class RoleController : ControllerBase
+{
+  private readonly IMediator _mediator;
+
+  public RoleController(IMediator mediator)
+  {
+    _mediator = mediator;
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<RoleModel>> CreateAsync([FromBody] CreateOrReplaceRolePayload payload, CancellationToken cancellationToken)
+  {
+    CreateOrReplaceRoleCommand command = new(Id: null, payload, Version: null);
+    CreateOrReplaceRoleResult result = await _mediator.Send(command, cancellationToken);
+    return ToActionResult(result);
+  }
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<RoleModel>> ReadAsync(Guid id, CancellationToken cancellationToken)
+  {
+    ReadRoleQuery command = new(id, UniqueName: null);
+    RoleModel? role = await _mediator.Send(command, cancellationToken);
+    return role == null ? NotFound() : Ok(role);
+  }
+
+  [HttpGet("name:{uniqueName}")]
+  public async Task<ActionResult<RoleModel>> ReadAsync(string uniqueName, CancellationToken cancellationToken)
+  {
+    ReadRoleQuery command = new(Id: null, uniqueName);
+    RoleModel? role = await _mediator.Send(command, cancellationToken);
+    return role == null ? NotFound() : Ok(role);
+  }
+
+  [HttpPut("{id}")]
+  public async Task<ActionResult<RoleModel>> ReplaceAsync(Guid id, [FromBody] CreateOrReplaceRolePayload payload, long? version, CancellationToken cancellationToken)
+  {
+    CreateOrReplaceRoleCommand command = new(id, payload, version);
+    CreateOrReplaceRoleResult result = await _mediator.Send(command, cancellationToken);
+    return ToActionResult(result);
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<SearchResults<RoleModel>>> SearchAsync([FromQuery] SearchRolesParameters parameters, CancellationToken cancellationToken)
+  {
+    SearchRolesPayload payload = parameters.ToPayload();
+    SearchRolesQuery query = new(payload);
+    SearchResults<RoleModel> roles = await _mediator.Send(query, cancellationToken);
+    return Ok(roles);
+  }
+
+  [HttpPatch("{id}")]
+  public async Task<ActionResult<RoleModel>> UpdateAsync(Guid id, [FromBody] UpdateRolePayload payload, CancellationToken cancellationToken)
+  {
+    UpdateRoleCommand command = new(id, payload);
+    RoleModel? role = await _mediator.Send(command, cancellationToken);
+    return role == null ? NotFound() : Ok(role);
+  }
+
+  private ActionResult<RoleModel> ToActionResult(CreateOrReplaceRoleResult result)
+  {
+    if (result.Role == null)
+    {
+      return NotFound();
+    }
+    else if (result.Created)
+    {
+      Uri uri = new($"{Request.Scheme}://{Request.Host}/api/roles/{result.Role.Id}", UriKind.Absolute);
+      return Created(uri, result.Role);
+    }
+    return Ok(result.Role);
+  }
+}
