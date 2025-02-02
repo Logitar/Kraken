e01f86eff1aef1d031247cabe791c984a90db38a
Francis Pion <francispion@hotmail.com>
2025-02-02 17:14:29 -0500

    contracts

---
+++ backend/src/Logitar.Kraken.Contracts/Users/ChangePasswordPayload.cs
@@ -0,0 +1,16 @@
+﻿namespace Logitar.Kraken.Contracts.Users;
+
+public record ChangePasswordPayload
+{
+  public string? Current { get; set; }
+  public string New { get; set; } = string.Empty;
+
+  public ChangePasswordPayload()
+  {
+  }
+
+  public ChangePasswordPayload(string newPassword)
+  {
+    New = newPassword;
+  }
+}
---
+++ backend/src/Logitar.Kraken.Contracts/Users/SearchUsersPayload.cs
@@ -0,0 +1,13 @@
+﻿using Logitar.Kraken.Contracts.Search;
+
+namespace Logitar.Kraken.Contracts.Users;
+
+public record SearchUsersPayload : SearchPayload
+{
+  public bool? HasAuthenticated { get; set; }
+  public bool? HasPassword { get; set; }
+  public bool? IsDisabled { get; set; }
+  public bool? IsConfirmed { get; set; }
+
+  public new List<UserSortOption> Sort { get; set; } = [];
+}
