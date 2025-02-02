f0aee2dac8bae522ff0cee6a9dc4a7d178cf32c3
Francis Pion <francispion@hotmail.com>
2025-02-01 21:25:21 -0500

    Added some controllers.

--- backend/src/Logitar.Kraken.Contracts/ApiKeys/ApiKeyStatus.cs
+++ backend/src/Logitar.Kraken.Contracts/ApiKeys/ApiKeyStatus.cs
@@ -4,4 +4,14 @@ public record ApiKeyStatus
 {
   public bool IsExpired { get; set; }
   public DateTime? Moment { get; set; }
+
+  public ApiKeyStatus()
+  {
+  }
+
+  public ApiKeyStatus(bool isExpired, DateTime? moment = null)
+  {
+    IsExpired = isExpired;
+    Moment = moment;
+  }
 }
