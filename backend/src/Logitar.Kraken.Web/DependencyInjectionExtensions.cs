1e31ef593528f0a4deb02b5846a3fc0e2097078a
Francis Pion <francispion@hotmail.com>
2025-02-01 21:05:24 -0500

    Implemented a Web API. (#14)

---
+++ backend/src/Logitar.Kraken.Web/DependencyInjectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Logitar.Kraken.Core;
+
+namespace Logitar.Kraken.Web;
+
+public static class DependencyInjectionExtensions
+{
+  public static IServiceCollection AddLogitarKrakenWeb(this IServiceCollection services, IConfiguration configuration)
+  {
+    services.AddControllersWithViews(/*options => options.Filters.Add<LoggingFilter>()*/) // TODO(fpion): Logging
+      .AddJsonOptions(options =>
+      {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+      });
+
+    //CookiesSettings cookiesSettings = configuration.GetSection(CookiesSettings.SectionKey).Get<CookiesSettings>() ?? new();
+    //services.AddSingleton(cookiesSettings); // TODO(fpion): Session
+
+    //CorsSettings corsSettings = configuration.GetSection(CorsSettings.SectionKey).Get<CorsSettings>() ?? new();
+    //services.AddSingleton(corsSettings); // TODO(fpion): CORS
+
+    //OpenAuthenticationSettings openAuthenticationSettings = configuration.GetSection(OpenAuthenticationSettings.SectionKey).Get<OpenAuthenticationSettings>() ?? new();
+    //services.AddSingleton(openAuthenticationSettings); // TODO(fpion): Authentication
+
+    return services
+      .AddSingleton<IApplicationContext, HttpApplicationContext>()
+      /*.AddTransient<IOpenAuthenticationService, OpenAuthenticationService>()*/; // TODO(fpion): Authentication
+  }
+}
---
+++ backend/src/Logitar.Kraken/Controllers/IndexController.cs
@@ -0,0 +1,12 @@
+﻿using Logitar.Kraken.Models.Index;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logitar.Kraken.Controllers;
+
+[ApiController]
+[Route("api")]
+public class IndexController : ControllerBase
+{
+  [HttpGet]
+  public ActionResult<ApiVersion> Get() => Ok(ApiVersion.Current);
+}
