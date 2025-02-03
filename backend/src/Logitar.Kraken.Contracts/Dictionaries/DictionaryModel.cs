adb889000e9a190d05eb308c5713a4736c773771
Francis Pion <francispion@hotmail.com>
2025-02-03 16:38:41 -0500

    contracts

---
+++ backend/src/Logitar.Kraken.Contracts/Dictionaries/DictionaryModel.cs
@@ -0,0 +1,14 @@
+﻿using Logitar.Kraken.Contracts.Localization;
+using Logitar.Kraken.Contracts.Realms;
+
+namespace Logitar.Kraken.Contracts.Dictionaries;
+
+public class DictionaryModel : AggregateModel
+{
+  public LanguageModel Language { get; set; } = new();
+
+  public int EntryCount { get; set; }
+  public List<DictionaryEntryModel> Entries { get; set; } = [];
+
+  public RealmModel? Realm { get; set; }
+}
---
+++ backend/src/Logitar.Kraken.Contracts/Dictionaries/DictionarySortOption.cs
@@ -0,0 +1,20 @@
+﻿using Logitar.Kraken.Contracts.Search;
+
+namespace Logitar.Kraken.Contracts.Dictionaries;
+
+public record DictionarySortOption : SortOption
+{
+  public new DictionarySort Field
+  {
+    get => Enum.Parse<DictionarySort>(base.Field);
+    set => base.Field = value.ToString();
+  }
+
+  public DictionarySortOption() : this(DictionarySort.UpdatedOn, isDescending: true)
+  {
+  }
+
+  public DictionarySortOption(DictionarySort field, bool isDescending = false) : base(field.ToString(), isDescending)
+  {
+  }
+}
