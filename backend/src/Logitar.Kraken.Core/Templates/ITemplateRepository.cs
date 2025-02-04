f5a256df1395716b688c14c7988322a62babb4bb
Francis Pion <francispion@hotmail.com>
2025-02-03 22:38:19 -0500

    domain

---
+++ backend/src/Logitar.Kraken.Core/Templates/ITemplateRepository.cs
@@ -0,0 +1,23 @@
+﻿using Logitar.Kraken.Core.Realms;
+
+namespace Logitar.Kraken.Core.Templates;
+
+public interface ITemplateRepository
+{
+  Task<Template?> LoadAsync(TemplateId id, CancellationToken cancellationToken = default);
+  Task<Template?> LoadAsync(TemplateId id, long? version, CancellationToken cancellationToken = default);
+  Task<Template?> LoadAsync(TemplateId id, bool? isDeleted, CancellationToken cancellationToken = default);
+  Task<Template?> LoadAsync(TemplateId id, bool? isDeleted, long? version, CancellationToken cancellationToken = default);
+
+  Task<IReadOnlyCollection<Template>> LoadAsync(CancellationToken cancellationToken = default);
+  Task<IReadOnlyCollection<Template>> LoadAsync(bool? isDeleted, CancellationToken cancellationToken = default);
+
+  Task<IReadOnlyCollection<Template>> LoadAsync(IEnumerable<TemplateId> ids, CancellationToken cancellationToken = default);
+  Task<IReadOnlyCollection<Template>> LoadAsync(IEnumerable<TemplateId> ids, bool? isDeleted, CancellationToken cancellationToken = default);
+
+  Task<IReadOnlyCollection<Template>> LoadAsync(RealmId? realmId, CancellationToken cancellationToken = default);
+  Task<Template?> LoadAsync(RealmId? realmId, Identifier uniqueKey, CancellationToken cancellationToken = default);
+
+  Task SaveAsync(Template template, CancellationToken cancellationToken = default);
+  Task SaveAsync(IEnumerable<Template> templates, CancellationToken cancellationToken = default);
+}
