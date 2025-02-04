54162e0b1613955575b1ed4128dc975d9d5f8419
Francis Pion <francispion@hotmail.com>
2025-02-04 13:50:36 -0500

    Added optional UserId to One-Time Passwords (OTP).

--- backend/src/Logitar.Kraken.Core/Passwords/Commands/CreateOneTimePasswordCommand.cs
+++ backend/src/Logitar.Kraken.Core/Passwords/Commands/CreateOneTimePasswordCommand.cs
@@ -3,6 +3,8 @@ using Logitar.EventSourcing;
 using Logitar.Kraken.Contracts;
 using Logitar.Kraken.Contracts.Passwords;
 using Logitar.Kraken.Core.Passwords.Validators;
+using Logitar.Kraken.Core.Realms;
+using Logitar.Kraken.Core.Users;
 using MediatR;
 
 namespace Logitar.Kraken.Core.Passwords.Commands;
@@ -15,17 +17,20 @@ internal class CreateOneTimePasswordCommandHandler : IRequestHandler<CreateOneTi
   private readonly IOneTimePasswordQuerier _oneTimePasswordQuerier;
   private readonly IOneTimePasswordRepository _oneTimePasswordRepository;
   private readonly IPasswordManager _passwordManager;
+  private readonly IUserManager _userManager;
 
   public CreateOneTimePasswordCommandHandler(
     IApplicationContext applicationContext,
     IOneTimePasswordQuerier oneTimePasswordQuerier,
     IOneTimePasswordRepository oneTimePasswordRepository,
-    IPasswordManager passwordManager)
+    IPasswordManager passwordManager,
+    IUserManager userManager)
   {
     _applicationContext = applicationContext;
     _oneTimePasswordQuerier = oneTimePasswordQuerier;
     _oneTimePasswordRepository = oneTimePasswordRepository;
     _passwordManager = passwordManager;
+    _userManager = userManager;
   }
 
   public async Task<OneTimePasswordModel> Handle(CreateOneTimePasswordCommand command, CancellationToken cancellationToken)
@@ -33,11 +38,14 @@ internal class CreateOneTimePasswordCommandHandler : IRequestHandler<CreateOneTi
     CreateOneTimePasswordPayload payload = command.Payload;
     new CreateOneTimePasswordValidator().ValidateAndThrow(payload);
 
-    OneTimePasswordId oneTimePasswordId = OneTimePasswordId.NewId(_applicationContext.RealmId);
+    ActorId? actorId = _applicationContext.ActorId;
+    RealmId? realmId = _applicationContext.RealmId;
+
+    OneTimePasswordId oneTimePasswordId = OneTimePasswordId.NewId(realmId);
     OneTimePassword? oneTimePassword;
     if (payload.Id.HasValue)
     {
-      oneTimePasswordId = new(oneTimePasswordId.RealmId, payload.Id.Value);
+      oneTimePasswordId = new(realmId, payload.Id.Value);
       oneTimePassword = await _oneTimePasswordRepository.LoadAsync(oneTimePasswordId, cancellationToken);
       if (oneTimePassword != null)
       {
@@ -45,10 +53,11 @@ internal class CreateOneTimePasswordCommandHandler : IRequestHandler<CreateOneTi
       }
     }
 
-    ActorId? actorId = _applicationContext.ActorId;
-
     Password password = _passwordManager.Generate(payload.Characters, payload.Length, out string passwordString);
-    oneTimePassword = new(password, payload.ExpiresOn, payload.MaximumAttempts, actorId, oneTimePasswordId);
+    User? user = string.IsNullOrWhiteSpace(payload.User)
+      ? null
+      : await _userManager.FindAsync(payload.User, nameof(payload.User), includeId: true, cancellationToken);
+    oneTimePassword = new(password, payload.ExpiresOn, payload.MaximumAttempts, user?.Id, actorId, oneTimePasswordId);
 
     foreach (CustomAttributeModel customAttribute in payload.CustomAttributes)
     {
