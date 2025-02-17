078a35440435dcc0266bb5c055d7165ef2e25de9
Francis Pion <francispion@hotmail.com>
2025-02-16 22:13:06 -0500

    frontend

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -2,7 +2,6 @@
 interface ImportMetaEnv {
   readonly VITE_APP_API_BASE_URL?: string;
   readonly VITE_APP_BASE_URL?: string;
-  readonly VITE_APP_ENABLE_GRAPHQL?: string;
   readonly VITE_APP_ENABLE_OPENAPI?: string;
   readonly VITE_APP_VERSION: string;
 }
