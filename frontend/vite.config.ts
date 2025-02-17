abf1cc02b9eb316dffa0487b07f3a0e74d21deab
Francis Pion <francispion@hotmail.com>
2025-02-16 21:34:49 -0500

    Frontend.

---
+++ frontend/vite.config.ts
@@ -0,0 +1,34 @@
+import VueDevTools from 'vite-plugin-vue-devtools'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+import packageJson from './package.json'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), VueDevTools()],
+  define: {
+    'import.meta.env.VITE_APP_VERSION': JSON.stringify(packageJson.version),
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: '../backend/src/Logitar.Kraken.Web/wwwroot',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+  server: {
+    port: 7746,
+  },
+})
