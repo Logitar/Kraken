7dad082a398ae8d3f0a7d392899a1275f2e79a2e
Francis Pion <francispion@hotmail.com>
2025-02-17 12:35:28 -0500

    fa

--- frontend/src/fontAwesome.ts
+++ frontend/src/fontAwesome.ts
@@ -2,9 +2,9 @@ import type { App } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faArrowRightToBracket, faUser, faVial } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRightFromBracket, faArrowRightToBracket, faUser, faVial } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faArrowRightToBracket, faUser, faVial);
+library.add(faArrowRightFromBracket, faArrowRightToBracket, faUser, faVial);
 
 export default function (app: App) {
   app.component("font-awesome-icon", FontAwesomeIcon);
